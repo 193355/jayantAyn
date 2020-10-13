9c1a25be6843e37d52844e9f74740a037d38bd4d
rajesh <meena.utis.rajesh@gmail.com>
2020-10-13 11:50:38 +0530

    commit

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -38,7 +38,7 @@ export class NavComponent implements OnInit {
     }
     )
 
-    // Login form group -
+    // Login form group
     this.loginForm = this.formBuilder.group({
       email: ['', Validators.compose([Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")])],
       password: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
